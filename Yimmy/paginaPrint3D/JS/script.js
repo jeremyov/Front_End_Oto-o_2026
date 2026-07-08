c83a8a5888d84fdb903c36d9b186034c3595ca01
Yimmy <jeremyo.villar@gmail.es>
2026-07-07 23:18:57 -0400

    página terminada

---
+++ Yimmy/paginaPrint3D/JS/script.js
@@ -0,0 +1,16 @@
+
+let currentIndex = 0
+
+function moveSlide(button, direction) {
+    const slider = button.parentElement;
+    const slides = slider.querySelector('.slides');
+    const totalSlides = slides.children.length;
+
+    let currentIndex = Number(slider.dataset.index || 0);
+
+    currentIndex = (currentIndex + direction + totalSlides) % totalSlides;
+    slides.style.transform = `translateX(-${currentIndex * 100}%)`;
+    slider.dataset.index = currentIndex;
+}
+
+
--- Yimmy/paginaPrint3D/JS/scriptContacto.js
+++ Yimmy/paginaPrint3D/JS/scriptContacto.js
@@ -1,18 +1,3 @@
-
-let currentIndex = 0
-
-function moveSlide(button, direction) {
-    const slider = button.parentElement;
-    const slides = slider.querySelector('.slides');
-    const totalSlides = slides.children.length;
-
-    let currentIndex = Number(slider.dataset.index || 0);
-
-    currentIndex = (currentIndex + direction + totalSlides) % totalSlides;
-    slides.style.transform = `translateX(-${currentIndex * 100}%)`;
-    slider.dataset.index = currentIndex;
-}
-
 function esEmailValido(email) {
     let regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return regex.test(email);
